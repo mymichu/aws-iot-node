1d104d499b07cd5063c2a3f57212982512317bb6
mymichu <meyer_michel@outlook.com>
2021-05-26 15:11:37 +0000

    refactor freertos_ios

--- freertos_iot_node/src/main.c
+++ freertos_iot_node/src/main.c
@@ -40,7 +40,7 @@ static void generator(void)
     {
         value++;
         xQueueSend( xQueue, &value , 1000 );
-        vTaskDelay(500);
+        vTaskDelay(100);
         if(value==100){
           value=10;
         }
@@ -127,101 +127,3 @@ void assert_failed(char* file, uint32_t line)
 
 }
 
-void vApplicationMallocFailedHook()
-{
-    taskDISABLE_INTERRUPTS();
-    /* Loop forever */
-    for( ; ; )
-    {
-    }
-}
-
-
-void vApplicationStackOverflowHook( TaskHandle_t xTask,
-                                    char * pcTaskName )
-{
-    portDISABLE_INTERRUPTS();
-    /* Unused Parameters */
-    ( void ) xTask;
-    ( void ) pcTaskName;
-    /* Loop forever */
-    for( ; ; )
-    {
-    }
-}
-
-void vApplicationIdleHook( void )
-{
-volatile size_t xFreeStackSpace;
-
-    /* The idle task hook is enabled by setting configUSE_IDLE_HOOK to 1 in
-    FreeRTOSConfig.h.
-
-    This function is called on each cycle of the idle task.  In this case it
-    does nothing useful, other than report the amount of FreeRTOS heap that
-    remains unallocated. */
-    xFreeStackSpace = xPortGetFreeHeapSize();
-
-    if( xFreeStackSpace > 100 )
-    {
-        /* By now, the kernel has allocated everything it is going to, so
-        if there is a lot of heap remaining unallocated then
-        the value of configTOTAL_HEAP_SIZE in FreeRTOSConfig.h can be
-        reduced accordingly. */
-    }
-}
-
-void vApplicationGetIdleTaskMemory( StaticTask_t **ppxIdleTaskTCBBuffer,
-                                    StackType_t **ppxIdleTaskStackBuffer,
-                                    uint32_t *pulIdleTaskStackSize )
-{
-/* If the buffers to be provided to the Idle task are declared inside this
-function then they must be declared static - otherwise they will be allocated on
-the stack and so not exists after this function exits. */
-static StaticTask_t xIdleTaskTCB;
-static StackType_t uxIdleTaskStack[ configMINIMAL_STACK_SIZE ];
-
-    /* Pass out a pointer to the StaticTask_t structure in which the Idle task's
-    state will be stored. */
-    *ppxIdleTaskTCBBuffer = &xIdleTaskTCB;
-
-    /* Pass out the array that will be used as the Idle task's stack. */
-    *ppxIdleTaskStackBuffer = uxIdleTaskStack;
-
-    /* Pass out the size of the array pointed to by *ppxIdleTaskStackBuffer.
-    Note that, as the array is necessarily of type StackType_t,
-    configMINIMAL_STACK_SIZE is specified in words, not bytes. */
-    *pulIdleTaskStackSize = configMINIMAL_STACK_SIZE;
-}
-/*-----------------------------------------------------------*/
-
-/* configSUPPORT_STATIC_ALLOCATION and configUSE_TIMERS are both set to 1, so the
-application must provide an implementation of vApplicationGetTimerTaskMemory()
-to provide the memory that is used by the Timer service task. */
-void vApplicationGetTimerTaskMemory( StaticTask_t **ppxTimerTaskTCBBuffer,
-                                     StackType_t **ppxTimerTaskStackBuffer,
-                                     uint32_t *pulTimerTaskStackSize )
-{
-/* If the buffers to be provided to the Timer task are declared inside this
-function then they must be declared static - otherwise they will be allocated on
-the stack and so not exists after this function exits. */
-static StaticTask_t xTimerTaskTCB;
-static StackType_t uxTimerTaskStack[ configTIMER_TASK_STACK_DEPTH ];
-
-    /* Pass out a pointer to the StaticTask_t structure in which the Timer
-    task's state will be stored. */
-    *ppxTimerTaskTCBBuffer = &xTimerTaskTCB;
-
-    /* Pass out the array that will be used as the Timer task's stack. */
-    *ppxTimerTaskStackBuffer = uxTimerTaskStack;
-
-    /* Pass out the size of the array pointed to by *ppxTimerTaskStackBuffer.
-    Note that, as the array is necessarily of type StackType_t,
-    configTIMER_TASK_STACK_DEPTH is specified in words, not bytes. */
-    *pulTimerTaskStackSize = configTIMER_TASK_STACK_DEPTH;
-}
-
-void vApplicationDaemonTaskStartupHook( void )
-{
-
-}
